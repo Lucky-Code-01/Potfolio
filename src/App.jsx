fa08f36978b03a398520b351cae8cf826aceeff0
lucky-code-01 <luckybaisoya003@gmail.com>
2025-06-01 15:55:36 +0530

    add Scrolling feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,13 @@ import About from './Component/About/About'
 import Skills from './Component/Skills/Skills'
 import Contact from './Component/Contact/Contact'
 import Footer from './Component/Footer/Footer'
+import { useScroll,motion } from 'framer-motion'
 export default function App() {
+  const {scrollYProgress} = useScroll();
   return (
     <div className='bg-[#0F172A] text-gray-300 min-h-screen w-full overflow-hidden'>
+      <motion.div id="scroll" style={{scaleX:scrollYProgress}}>
+      </motion.div>
       <Navbar/>
       <Hero/>
       <Middle/>
